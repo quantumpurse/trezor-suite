901aa870d052d4ef880a33c98448a88f2baf67bf
DinhMaiMTD <142642094+DinhMaiMTD@users.noreply.github.com>
2026-04-27 14:13:10 +0700

    fix(ckb): re-implement protobuf messages in definitions/ structure

--- packages/protobuf/src/definitions/index.ts
+++ packages/protobuf/src/definitions/index.ts
@@ -63,6 +63,15 @@ import {
     CardanoTxWitnessResponse,
 } from './messages-cardano';
 import {
+    CKBAddress,
+    CKBGetAddress,
+    CKBSignTx,
+    CKBTxAckCellDep,
+    CKBTxAckInput,
+    CKBTxAckOutput,
+    CKBTxRequest,
+} from './messages-ckb';
+import {
     ButtonAck,
     ButtonRequest,
     Deprecated_PassphraseStateAck,
@@ -305,6 +314,7 @@ export * from './messages-bitcoin';
 export * from './messages-ble';
 export * from './messages-bootloader';
 export * from './messages-cardano';
+export * from './messages-ckb';
 export * from './messages-crypto';
 export * from './messages-management';
 export * from './messages-debug';
@@ -396,6 +406,13 @@ export const MessageType = Type.Object(
         CardanoMessageDataRequest,
         CardanoMessageDataResponse,
         CardanoMessageSignature,
+        CKBGetAddress,
+        CKBAddress,
+        CKBSignTx,
+        CKBTxRequest,
+        CKBTxAckInput,
+        CKBTxAckOutput,
+        CKBTxAckCellDep,
         CipherKeyValue,
         CipheredKeyValue,
         SignIdentity,
@@ -648,6 +665,11 @@ export type WireInMessage =
     | 'CardanoTxHostAck'
     | 'CardanoSignMessageInit'
     | 'CardanoMessageDataResponse'
+    | 'CKBGetAddress'
+    | 'CKBSignTx'
+    | 'CKBTxAckInput'
+    | 'CKBTxAckOutput'
+    | 'CKBTxAckCellDep'
     | 'CipherKeyValue'
     | 'SignIdentity'
     | 'GetECDHSessionKey'
@@ -779,6 +801,8 @@ export type WireOutMessage =
     | 'CardanoSignTxFinished'
     | 'CardanoMessageDataRequest'
     | 'CardanoMessageSignature'
+    | 'CKBAddress'
+    | 'CKBTxRequest'
     | 'CipheredKeyValue'
     | 'SignedIdentity'
     | 'ECDHSessionKey'
